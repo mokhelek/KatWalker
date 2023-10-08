8df749057dc4da1ea433b82114e249494bad3065
mokhelek <mkatleho191@gmail.com>
2023-10-08 17:05:15 +0200

    add product card + API connection

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function ProductCard({shoe}) {
+    return (
+        <>
+            <div style={{ padding: "0.4rem" }} className="col-lg-4 col-md-3 col-6 shoe-box-wrapper">
+                <div className="shoe-box card">
+                    <img style={{ height: "15rem" }} className="img-fluid" src={shoe.image_url} alt="" />
+                    <div className="shoe-card-body card-body">
+                        <h6 style={{ marginBottom: "0rem", fontWeight: "600" }}>{shoe.shoe_name}</h6>
+                        <small style={{ color: "gray" }}>{shoe.brand}</small>
+
+                        <div className="row">
+                            <div className="col-sm-7">
+                                <p style={{ fontSize: "0.9rem" }}>
+                                    R <span> 2000</span>
+                                </p>
+                            </div>
+                            <div className="col-sm-5">
+                                <p style={{ color: "rgb(190, 123, 0)", fontSize: "0.9rem" }}>
+                                    <span>{shoe.stock_quantity}</span> in stock
+                                </p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <button style={{ width: "100%", borderRadius: "0" }} className="btn btn-dark btn-block btn-sm">
+                                add to cart
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default ProductCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import ProductCard from "../components/ProductCard";
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
 
 function Home() {
-  return (
-    <h1><center>Home page</center></h1>
-  )
+    const [shoes, setShoes] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("https://shoe-catalogue-api.onrender.com/api/shoes")
+            .then((response) => {
+                setShoes(response.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }, []);
+
+    return (
+        <>
+            <div style={{width:"90%"}} className="container-fluid">
+                <div className="row">
+                    <div className="col-3"></div>
+                    <div className="col-9">
+                        <div className="row">
+                            {shoes.map((shoe) => (
+                                <ProductCard shoe={shoe} />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default Home
+export default Home;
