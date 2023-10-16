7c26392437ee3323ed3f20719ef08d62e9cfc331
mokhelek <mkatleho191@gmail.com>
2023-10-16 14:43:06 +0200

    add  option selection highlight for color filtering

--- src/components/ColorOption.jsx
+++ src/components/ColorOption.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 
 function ColorOption({ onSetColor, option }) {
-    const handleColorFilter = () => {
+
+    const handleColorFilter = (event) => {
         onSetColor(option);
+        selectedColor(event.target);
     };
+
+    const selectedColor = (element) => {
+        let elem = document.querySelectorAll(".selected");
+        for (let i of elem){
+            i.classList.remove("selected");
+        }
+        element.classList.add("selected");
+    };
+
 
     return (
         <>
-            <button onClick={handleColorFilter} className="btn color-options" style={{ marginBottom: "0.4rem" }}>
+            <button onClick={handleColorFilter} className="btn color" style={{ marginBottom: "0.4rem" }}>
                 <div style={{ height: "2rem", width: "2rem", borderRadius: "5px", border: "1px black solid", backgroundColor: `${option}` }}></div>
             </button>
         </>
