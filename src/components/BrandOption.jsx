c96c50c2abd853c4e81413794adbe45f2b98c724
mokhelek <mkatleho191@gmail.com>
2023-10-16 11:35:54 +0200

    add display of currently selected brand

--- src/components/BrandOption.jsx
+++ src/components/BrandOption.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
 function BrandOption({ onSetBrand, option }) {
-    
-    const handleBrandFilter = () => {
+
+    const handleBrandFilter = (event) => {
         onSetBrand(option);
+        selectedBrand(event.target);
+    };
+
+    const selectedBrand = (element) => {
+        let elem = document.querySelectorAll(".brand-options");
+        for (let i of elem){
+            i.classList.remove("selected-brand");
+        }
+        element.classList.add("selected-brand");
     };
 
     return (
         <>
-            <button onClick={handleBrandFilter} className="btn btn-outline-secondary btn-sm" style={{ borderRadius: "0", padding: "0.1rem 0.85rem", marginBottom: "0.4rem", marginLeft: "0.4rem" }}>
+            <button onClick={handleBrandFilter} className="btn btn-outline-secondary btn-sm brand-options" style={{ borderRadius: "0", padding: "0.1rem 0.85rem", marginBottom: "0.4rem", marginLeft: "0.4rem" }}>
                 {option}
             </button>
         </>
